96f6b21579f47f712967af4b6384f6d86a06f7e6
zoro <641184747@qq.com>
2017-04-12 21:01:10 +0800

    update code .

---
+++ jvdaily2/src/main/java/com/jv/daily/ui/content/ContentActivity.java
@@ -0,0 +1,26 @@
+package com.jv.daily.ui.content;
+
+import com.jv.daily.base.app.AppComponent;
+import com.jv.daily.base.mvp.BaseActivity;
+
+/**
+ * Created by 64118 on 2017/4/12.
+ */
+
+public class ContentActivity extends BaseActivity{
+
+    @Override
+    protected int bindRootView() {
+        return 0;
+    }
+
+    @Override
+    protected void bindData() {
+
+    }
+
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+}

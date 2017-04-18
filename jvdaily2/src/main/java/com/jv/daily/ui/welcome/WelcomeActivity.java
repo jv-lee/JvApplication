755d5b897e679207f7cabc09f34dc3fa8783544c
zoro <641184747@qq.com>
2017-04-18 20:41:51 +0800

    update MainPresenter date code ->

--- jvdaily2/src/main/java/com/jv/daily/ui/welcome/WelcomeActivity.java
+++ jvdaily2/src/main/java/com/jv/daily/ui/welcome/WelcomeActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.jv.daily.R;
 import com.jv.daily.service.NewsService;
 import com.jv.daily.ui.main.MainActivity;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Administrator on 2017/4/18.
@@ -20,16 +26,23 @@ public class WelcomeActivity extends AppCompatActivity {
 
     @Inject
     NewsService newsService;
+    @BindView(R.id.iv_bg)
+    ImageView ivBg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        ButterKnife.bind(this);
+
+        Glide.with(this).load(R.drawable.welcome_icon).into(ivBg);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
             }
         }, 2000);
 

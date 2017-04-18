e65ecbfd57070d4b07c656e76018c92a17562f23
jv-lee <jv.lee@foxmail>
2017-04-18 19:06:19 +0800

    insert gradle version ->

--- jvdaily2/src/main/java/com/jv/daily/ui/welcome/WelcomeActivity.java
+++ jvdaily2/src/main/java/com/jv/daily/ui/welcome/WelcomeActivity.java
@@ -1,11 +1,14 @@
 package com.jv.daily.ui.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jv.daily.R;
 import com.jv.daily.service.NewsService;
+import com.jv.daily.ui.main.MainActivity;
 
 import javax.inject.Inject;
 
@@ -22,6 +25,13 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            }
+        }, 2000);
 
     }
 }
